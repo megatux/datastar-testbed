2b601011a1e1e997c7e2cfd1784062bcbdd0bbdb
Cristian Molina <megatux@gmail.com>
2025-05-15 12:46:27 -0300

    Add dummy stream page

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -3,9 +3,26 @@ class ExamplesController < ApplicationController
     render Index.new
   end
 
+  def stream01
+    render Stream01.new
+  end
+
   class Index < ::Views::Base
     def view_template
-      h1 { "hi" }
+      h1 { "Datastar Examples" }
+      ul {
+        li {
+          a(href: "stream01") { "Stream example 01" }
+        }
+      }
     end
   end
+
+  class Stream01 < ::Views::Base
+    def view_template
+      h1 { "Stream Example 01" }
+      p { "Nothing yet" }
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "examples#index"
+
+  get "stream01", to: "examples#stream01"
 end
