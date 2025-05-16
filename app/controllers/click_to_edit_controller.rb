58bebdd98edb7fcc46952f9c09fafd232e9692dc
Cristian Molina <megatux@gmail.com>
2025-05-16 12:42:05 -0300

    Fix several issues

--- app/controllers/click_to_edit_controller.rb
+++ app/controllers/click_to_edit_controller.rb
@@ -1,41 +1,50 @@
 class ClickToEditController < ApplicationController
-  def index = render Index.new
+  def index
+    render Index.new
+  end
+
+  def show
+    if datastar.sse?
+      datastar.merge_fragments(Show.new(params[:id]))
+    else
+      render ViewWithComponent.new(Show.new(params[:id]))
+    end
+  end
 
   def edit
-    datastar = Datastar.new(request:, response:, view_context:)
-
-    # byebug
-    # datastar.merge_fragments(Edit.new, selector: "#contact_1")  # do not work
-    datastar.merge_fragments("<h1>hi!</h1>", selector: "#contact_1")  # works fine
+    datastar.merge_fragments(Edit.new(params[:id]))
   end
 
   def update
     redirect_to click_to_edit_index_path
   end
 
-  class Index < ::Views::Base
-    def view_template
-      div(id: "contact_1") {
-        label { "First Name: John" }
-        label { "Last Name: Doe" }
-        label { "Email: joe@blow.com" }
-        div {
-          button("data-on-click": "@get('/click_to_edit/1/edit')") {
-            plain "Edit"
-          }
+  private
 
-          button("data-on-click": "@get('/click_to_edit/1/contact_reset')") {
-            plain "Reset"
-          }
-        }
-      }
+  def datastar = @datastar ||= Datastar.new(request:, response:, view_context:)
+
+  class ViewWithComponent < Views::Base
+    def initialize(component)
+      @component = component
+    end
+
+    def view_template = render @component
+  end
+
+  class Index < Views::Base
+    def view_template
+      render Show.new(1)
     end
   end
 
-  class Edit < ::Views::Base
+  class Edit < Components::Base
+    def initialize(id)
+      @id = id
+    end
+
     def view_template
-      div(id: "contact_1", "data-signals" => "{
-              id: 1, firstName: 'John', lastName: 'Doe', email: 'joe@blow.com'}") do
+      div(id: "contact_#{@id}", "data-signals" => "{
+              id: #{@id}, firstName: 'John', lastName: 'Doe', email: 'joe@blow.com'}") do
         div(class: "form-control") do
           label { "First Name" }
           input type: "text", "data-bind" => "firstName"
@@ -49,15 +58,38 @@ class ClickToEditController < ApplicationController
           input type: "text", "data-bind" => "email"
         }
         div {
-          button("data-on-click" => "@put('/click_to_edit/1',"\
+          button("data-on-click" => "@put('#{click_to_edit_path(@id)}',"\
             "{ headers: { 'X-Csrf-Token': '#{helpers.form_authenticity_token}' }})") {
             plain "Save"
           }
-          button("data-on-click" => "@get('/click_to_edit/')") {
+          button("data-on-click" => "@get('/click_to_edit/#{@id}')") {
             plain "Cancel"
           }
         }
       end
+    end
+  end
+
+  class Show < Components::Base
+    def initialize(id)
+      @id = id
+    end
+
+    def view_template
+      div(id: "contact_#{@id}") {
+        label { "First Name: John" }
+        label { "Last Name: Doe" }
+        label { "Email: joe@blow.com" }
+        div {
+          button("data-on-click": "@get('/click_to_edit/#{@id}/edit')") {
+            plain "Edit"
+          }
+
+          button("data-on-click": "@get('/click_to_edit/#{@id}')") {
+            plain "Reset"
+          }
+        }
+      }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "examples#index"
 
-  resources :click_to_edit, only: [:index, :edit, :update]
+  resources :click_to_edit
   get "stream01", to: "examples#stream01"
   get "stream02", to: "examples#stream02"
 end
